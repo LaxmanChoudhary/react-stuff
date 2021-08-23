a245673caf81948b538b66a6d2ff95dd52fb4897
LaxmanChoudhary <laxmanchoudhary6811@gmail.com>
2021-08-23 23:49:26 +0530

    route resolve

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ export default class App extends Component {
         <BrowserRouter>
           <Layout>
             <Switch>
-              <Route exact path="/" component={Notes} />
-              <Route path="/create" component={CreateNote} />
+              <Route exact path="/react-stuff/" component={Notes} />
+              <Route path="/react-stuff/create" component={CreateNote} />
             </Switch>
           </Layout>
         </BrowserRouter>
